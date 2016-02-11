69b1fbbe8db75d50b1d7823465d77ef597dab654
Patrick Quirk <quirker@gmail.com>
2016-02-11 08:59:00 -0500

    Adjusted project output paths to put release artifacts in bin

--- src/PostBuild.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-rem $Id: PostBuild.bat,v 1.1 2005/02/17 22:47:25 jeffreyphillips Exp $
-rem post-build step in Visual Studio IDE to route outupt files
-rem to their proper directories
-move LibCurlNet.dll ..\bin
-move LibCurlNet.xml ..\doc
-
