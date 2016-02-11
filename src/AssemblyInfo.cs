503bc9ddd20c9b62333106673b856d754546ff4c
Patrick Quirk <quirker@gmail.com>
2016-02-11 09:45:14 -0500

    Updaing copyright info

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -3,13 +3,14 @@
  * Project: libcurl.NET
  *
  * Copyright (c) 2004, 2005 Jeff Phillips (jeff@jeffp.net)
+ *               2016 Patrick Quirk
  *
- * This software is licensed as described in the file COPYING, which you
+ * This software is licensed as described in the file LICENSE.md, which you
  * should have received as part of this distribution.
  *
  * You may opt to use, copy, modify, merge, publish, distribute and/or sell
  * copies of this Software, and permit persons to whom the Software is
- * furnished to do so, under the terms of the COPYING file.
+ * furnished to do so, under the terms of the LICENSE.md file.
  *
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF
  * ANY KIND, either express or implied.
@@ -18,20 +19,16 @@
  **************************************************************************/
 
 using System.Reflection;
-using System.Runtime.CompilerServices;
 
-[assembly:AssemblyCompany("Seaside Research")]
-[assembly:AssemblyCopyright("Copyright (c) 2005 Jeff Phillips")]
-
-[assembly:AssemblyProduct("libcurl.NET")]
-[assembly:AssemblyInformationalVersion("1.3.0.0")]
-
-[assembly:AssemblyFileVersion("1.3.0.0")]
-[assembly:AssemblyVersion("1.3.0.0")]
-[assembly:AssemblyTitle("libcurl.NET by Jeff Phillips")]
-[assembly:AssemblyDescription("libcurl binding for .NET")]
-
-[assembly:AssemblyCulture("")]
+[assembly: AssemblyCompany("Seaside Research")]
+[assembly: AssemblyCopyright("Copyright (c) 2005, 2016 Jeff Phillips, Patrick Quirk")]
+[assembly: AssemblyProduct("libcurl.NET")]
+[assembly: AssemblyInformationalVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyTitle("libcurl.NET by Jeff Phillips")]
+[assembly: AssemblyDescription("libcurl binding for .NET")]
+[assembly: AssemblyCulture("")]
 
 //[assembly:AssemblyDelaySign(false)]
 //[assembly:AssemblyKeyFile("SeasideResearch.keys")]
--- src/External.cs
+++ src/External.cs
@@ -4,12 +4,12 @@
  *
  * Copyright (c) 2004, 2005 Jeff Phillips (jeff@jeffp.net)
  *
- * This software is licensed as described in the file COPYING, which you
+ * This software is licensed as described in the file LICENSE.md, which you
  * should have received as part of this distribution.
  *
  * You may opt to use, copy, modify, merge, publish, distribute and/or sell
  * copies of this Software, and permit persons to whom the Software is
- * furnished to do so, under the terms of the COPYING file.
+ * furnished to do so, under the terms of the LICENSE.md file.
  *
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF
  * ANY KIND, either express or implied.
