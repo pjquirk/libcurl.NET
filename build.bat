bd23d8e45de9769e1f306d50be455ec83f68cdbf
Patrick Quirk <quirker@gmail.com>
2016-02-17 15:59:05 -0500

    Added a project for building/publishing
    This was inspired/borrowed from Moq's version.

--- build.bat
+++ build.bat
@@ -1,15 +1,9 @@
 @ECHO OFF
 SET MSBUILD="%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe"
-SET SOLUTION=LibCurlNet.sln
-SET PLATFORMS="x86","x64"
-SET CONFIGURATIONS=Debug,Release
+SET SOLUTION=build.proj
 
-FOR %%P IN (%PLATFORMS%) DO (
-FOR %%C IN (%CONFIGURATIONS%) DO (
-  %MSBUILD% %SOLUTION% /p:Configuration=%%C /p:Platform=%%P /target:Rebuild
-  IF %ERRORLEVEL% NEQ 0 GOTO :Fail
-)
-)
+%MSBUILD% %SOLUTION% /target:Publish
+IF %ERRORLEVEL% NEQ 0 GOTO :Fail
 
 GOTO :eof
 
